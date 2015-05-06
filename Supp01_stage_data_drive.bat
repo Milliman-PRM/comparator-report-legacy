94e4bfa4745e83dd2a356cd4d2117377e9a7e518
shea-parkes <shea.parkes@milliman.com>
2015-05-06 08:50:23 -0400

    Author convenience script to setup data drive during development.
    Do this so it's easier to short-circuit when you need to.

---
+++ Supp01_stage_data_drive.bat
@@ -0,0 +1,17 @@
+rem ### CODE OWNERS: Shea Parkes
+rem
+rem ### OBJECTIVE:
+rem   Provide a convenience script to setup the data drive.
+rem
+rem ### DEVELOPER NOTES:
+rem   It is not intended that this would be often utilized.
+rem   Most of the time, the python script below will be ran automatically as part of PRM.
+rem   However, during development, it might not already be ran, so this batch script provides
+rem     a convenience method of running it.
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+rem ### LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
+
+call activate prod3
+python "%~dp0\Post01_stage_data_drive.py"
