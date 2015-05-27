9069d962d1883c6368bef9758dc92b4bc3741e10
Michael Menser <Michael.Menser@milliman.com>
2015-05-27 12:05:45 -0400

    Added import os and import sys lines

--- Post099_Disk_Cleanup/Post01_disk_cleanup.py
+++ Post099_Disk_Cleanup/Post01_disk_cleanup.py
@@ -8,6 +8,8 @@
   This program imports 999_Disk_Cleanup from the HealthBI branch in order to clean up the disks.
 """
 
+import os
+import sys
 sys.path.append(os.path.join(os.environ['USERPROFILE'], 'HealthBI_LocalData'))
 import healthbi_env
 
@@ -16,5 +18,5 @@ import healthbi_env
 # =============================================================================
 
 if __name__ == '__main__':
-	from Prod01_disk_cleanup import dir_cleanup
-	dir_cleanup(healthbi_env.META["path_project_local"])
+    from Prod01_disk_cleanup import dir_cleanup
+    dir_cleanup(healthbi_env.META["path_project_local"])
