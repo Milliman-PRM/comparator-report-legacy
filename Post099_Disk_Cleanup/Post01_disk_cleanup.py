34a8ad8741a092ad2532870fa7b8a074dbd22322
Michael Menser <Michael.Menser@milliman.com>
2015-05-27 10:08:25 -0400

    if __name__ == '__main__':
    Added said line to the code in case parts of the code are imported.

--- Post099_Disk_Cleanup/Post01_disk_cleanup.py
+++ Post099_Disk_Cleanup/Post01_disk_cleanup.py
@@ -15,5 +15,6 @@ import healthbi_env
 # LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
 # =============================================================================
 
-from Prod01_disk_cleanup import dir_cleanup
-dir_cleanup(healthbi_env.META["path_project_local"])
+if __name__ == '__main__':
+	from Prod01_disk_cleanup import dir_cleanup
+	dir_cleanup(healthbi_env.META["path_project_local"])
