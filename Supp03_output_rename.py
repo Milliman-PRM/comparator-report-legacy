981ad35784c7ced66f084ae71d1b9b1ae29b927a
Aaron Burgess <Aaron.Burgess@milliman.com>
2016-01-18 14:11:57 -0500

    Raise error on invalid name extension
    Only 'all' or a positive integer are allowed

--- Supp03_output_rename.py
+++ Supp03_output_rename.py
@@ -11,6 +11,7 @@
 
 import os
 import sys
+import re
 import logging
 
 LOGGER = logging.getLogger(__file__)
@@ -20,11 +21,14 @@ sys.path.append(r"S:\Misc\_IndyMacros\Code\python\indypy")
 from file_utils import IndyPyPath
 
 _ANTI_PATTERN = r'.*(_all|_\d+)\.(sasb7dat|sqlite)'
+_EXT_PATTERN = r'^(all|\d+)$'
 
 
 def rename_files(directory, name_extension):
     """Target sas and sqlite files that do not feature
     an explicit name format"""
+    if not re.search(_EXT_PATTERN, name_extension, re.I):
+        raise ValueError("Name extensions must be 'all' or positive integer")
     path = IndyPyPath(directory)
     unwanted_files = path.collect_files_regex(_ANTI_PATTERN)
     all_ext_files = path.collect_files_extensions(['sasb7dat', 'sqlite'])
