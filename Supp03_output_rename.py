47f2287e1a6821e7084362c914557b864200af8e
Jason Altieri <Jason.Altieri@milliman.com>
2016-05-09 11:44:43 -0400

    update the renaming of the files
    Do this because we no longer limit to integer suffixes and the maximum
    allowable filename is 32 characters.

--- Supp03_output_rename.py
+++ Supp03_output_rename.py
@@ -22,14 +22,12 @@ _EXT_PATTERN = r'^(all|\d+)$'
 def rename_files(directory, name_extension):
     """Target sas and sqlite files that do not feature
     an explicit name format"""
-    if not re.search(_EXT_PATTERN, name_extension, re.I):
-        raise ValueError("Name extensions must be 'all' or positive integer")
     path = IndyPyPath(directory)
     unwanted_files = path.collect_files_regex(_ANTI_PATTERN)
     all_ext_files = path.collect_files_extensions(['sas7bdat', 'sqlite', 'sas7bndx'])
     final_files = list(set(all_ext_files) - set(unwanted_files))
     for file in final_files:
-        new_name = file.parent / file.stem
+        new_name = file.parent / file.stem[:32] / file.suffix
         new_file = '{file}_{name}{ext}'.format(file=str(new_name),
                                                name=name_extension, ext=file.suffix)
         os.rename(str(file), new_file)
