69dc1e18860e4fe5c8aea5525b0d616f0362e017
Jason Altieri <Jason.Altieri@milliman.com>
2016-05-13 11:06:10 -0400

    Update file searching pattern
    Do this because the output file pattern changed

--- Supp03_output_rename.py
+++ Supp03_output_rename.py
@@ -15,7 +15,7 @@ import re
 
 from indypy.file_utils import IndyPyPath
 
-_ANTI_PATTERN = r'.*(_all|_a_)\.(sas7bdat|sqlite|sas7bndx)'
+_ANTI_PATTERN = r'(_all|_a_).+?\.(sas7bdat|sqlite|sas7bndx)'
 
 def rename_files(directory, name_extension):
     """Target sas and sqlite files that do not feature
@@ -25,7 +25,10 @@ def rename_files(directory, name_extension):
     all_ext_files = path.collect_files_extensions(['sas7bdat', 'sqlite', 'sas7bndx'])
     final_files = list(set(all_ext_files) - set(unwanted_files))
     for file in final_files:
-        new_name = file.parent / file.stem[:32]
+        if file.suffix != '.sqlite':
+            new_name = file.parent / file.stem[:32]
+        else:
+            new_name = file.parent / file.stem
         new_file = '{file}_{name}{ext}'.format(file=str(new_name),
                                                name=name_extension, ext=file.suffix)
         os.rename(str(file), new_file)
