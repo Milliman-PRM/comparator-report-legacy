70183ccfe84c29651bd2a96d44284136d400c55c
Matthew.Hawthorne <matthew.hawthorne@milliman.com>
2017-08-01 15:52:05 -0400

    Decouple the generation of the providers table from the member table
    Do this to generate a client provider table

--- On005_Import_Assignment/Prod03_nextgen_import.py
+++ On005_Import_Assignment/Prod03_nextgen_import.py
@@ -68,7 +68,9 @@ _MEMBER_TIME_METADATA = {
                         }
 
 _PROVIDER_METADATA = {
-                        ''
+    'prv_name': {'label': 'ACO Provider'},
+    'prv_hier_1': {'label': 'Provider Primary Specialty'},
+    'prv_net_hier_1': {'label': 'ACO'}
 }
 
 # =============================================================================
@@ -382,41 +384,20 @@ def _load_mngreb_files(sparkapp: SparkApp, file_list: list, file_config: dict,
     )
 
 
-def build_providers(phys_df: DataFrame, npi_df: DataFrame) -> DataFrame:
+def build_providers(phys_df: DataFrame, npi_df: DataFrame, ref_taxonomy_df: DataFrame,
+                    performance_year: str, file_config: dict) -> DataFrame:
     """
 
     Args:
         phys_df:
         npi_df:
+        ref_taxonomy_df:
+        performance_year:
+        file_config:
 
     Returns:
 
     """
-
-
-def _build_client_all_member(ngalign_df: DataFrame, mngreb_df: DataFrame,
-                         phys_df: DataFrame, npi_df: DataFrame, file_config: dict)-> DataFrame:
-    """
-
-    Args:
-        ngalign_df:    DataFrame of stacked ngalign files
-        mngreb_df:     DataFrame of stacked mngreb files
-        phys_df:       DataFrame with physician files
-
-    Returns:
-        DataFrame with eligibility spans by member
-    """
-    performance_year = ngalign_df.select(
-        F.max(F.col('filedate')).alias('file_date')
-    ).collect()[0]['file_date']
-
-    changed_filedate_mngreb_df = mngreb_df.withColumnRenamed('filedate', 'mngreb_filedate')
-
-    member_exclusions_df = ngalign_df.join(changed_filedate_mngreb_df,
-                                           'hicno',
-                                           'left_outer'
-                                           )
-
     phys_window = Window.partitionBy(
         F.col('cur_clm_uniq_id'), F.col('bene_hic_num'), F.col('rndrg_prvdr_npi_num')
     ).orderBy(
@@ -458,6 +439,71 @@ def _build_client_all_member(ngalign_df: DataFrame, mngreb_df: DataFrame,
     ).where(
         (F.col('pcp_order') == 1) & (F.col('pcp_visit_count') > 0)
     )
+    reduce_npi_df = npi_df.select(
+        F.col("npi_npi"),
+        F.col('npi_prv_name_npi_prop').alias('prv_name'),
+        F.col('npi_prv_addr_line_1_npi').alias('prv_addr_street'),
+        F.split(F.col('npi_prv_addr_city_st_zip'), ',')[0].alias('prv_addr_city'),
+        F.col('npi_prv_addr_state').alias('prv_addr_state'),
+        F.col('npi_prv_addr_zip').alias('prv_addr_zip'),
+        F.col('npi_prv_specialty').alias('prv_specialty'),
+        F.col('npi_primary_taxonomy_cd').alias('prv_taxonomy_cd')
+    )
+    update_spec_df = reduce_npi_df.join(
+        ref_taxonomy_df.select(
+            F.col('taxonomy_code').alias('prv_taxonomy_cd'),
+            F.col('classification').alias('prv_hier_1')
+        ),
+        'prv_taxonomy_cd',
+        'left_outer'
+    ).withColumn(
+        'prv_hier_1',
+        F.coalesce(F.col('prv_hier_1'), F.lit('Unknown'))
+    ).drop('prv_taxonomy_cd')
+    return assign_phys_df.join(
+        update_spec_df,
+        assign_phys_df.rndrg_prv_dr_npi_num == update_spec_df.npi_npi,
+        'left_outer'
+    ).select(
+        F.col('bene_hic_num').alias('hicno'),
+        F.col('rndrg_prv_dr_npi_num').alias('prv_id'),
+        F.lit('NPI').alias('prv_id_name'),
+        F.lit(None).alias('prv_id_alt'),
+        F.lit(None).alias('prv_id_alt_name'),
+        F.col('prv_name'),
+        F.col('prv_addr_street'),
+        F.col('prv_addr_city'),
+        F.col('prv_addr_state'),
+        F.col('prv_addr_zip'),
+        F.col('prv_specialty_cd'),
+        F.col('prv_hier_1'),
+        F.lit(None).alias('prv_hier_2'),
+        F.lit(None).alias('prv_hier_3'),
+        F.lit(None).alias('prv_hier_4'),
+        F.lit(None).alias('prv_hier_5'),
+        F.lit('ACO').alias('prv_net_hier_1'),
+        F.lit(None).alias('prv_net_hier_2'),
+        F.lit(None).alias('prv_net_hier_3'),
+        F.lit('Y').alias('prv_net_aco_yn')
+    )
+
+
+def _build_client_all_member(ngalign_df: DataFrame, mngreb_df: DataFrame,
+                             provider_df: DataFrame)-> DataFrame:
+    """
+
+    Args:
+        ngalign_df:    DataFrame of stacked ngalign files
+        mngreb_df:     DataFrame of stacked mngreb files
+        provider_df:   Reduced DataFrame of the client provider table with HICNO
+
+    Returns:
+        DataFrame with eligibility spans by member
+    """
+
+    changed_filedate_mngreb_df = mngreb_df.withColumnRenamed('filedate', 'mngreb_filedate')
+
+    member_exclusions_df = ngalign_df.join(changed_filedate_mngreb_df, 'hicno', 'left_outer')
 
     base_member_assign_df = member_exclusions_df.withColumn(
         'date_start',
@@ -500,13 +546,13 @@ def _build_client_all_member(ngalign_df: DataFrame, mngreb_df: DataFrame,
     )
 
     base_member_phys_df = base_member_assign_df.join(
-        assign_phys_df,
-        base_member_assign_df.hicno == assign_phys_df.bene_hic_num,
+        provider_df.distinct(),
+        'hicno',
         'left_outer'
     ).withColumn(
         'mem_prv_id_align',
         F.when(
-            F.col('rndrg_prvdr_npi_num').isNull(),
+            F.col('prv_id').isNull(),
             F.when(
                 F.col('assignment_indicator').isin('Y'),
                 F.lit('Unknown')
@@ -514,7 +560,7 @@ def _build_client_all_member(ngalign_df: DataFrame, mngreb_df: DataFrame,
                 F.lit('Unassigned')
             )
         ).otherwise(
-            F.col('rndrg_prvdr_npi_num')
+            F.col('prv_id')
         )
     ).withColumn(
         'mem_addr_zip',
@@ -559,26 +605,7 @@ def _build_client_all_member(ngalign_df: DataFrame, mngreb_df: DataFrame,
                              "assigning physicians. Please check that the physician table "
                              "does not have duplication.")
 
-    reduced_npi_df = npi_df.select(
-        F.col('npi_npi'),
-        F.col('npi_prv_name_npi_prop')
-    ).distinct()
-
-    npi_join_df = base_member_phys_df.join(
-        reduced_npi_df,
-        base_member_phys_df.mem_prv_id_align == reduced_npi_df.npi_npi,
-        'left_outer'
-    ).withColumn(
-        'npi_prv_name_with_nulls',
-        F.when(
-            F.col('npi_prv_name_npi_prop').isNull(),
-            F.col('mem_prv_id_align')
-        ).otherwise(
-            F.col('npi_prv_name_npi_prop')
-        )
-    )
-
-    return npi_join_df.select(
+    return base_member_phys_df.select(
         F.col('hicno').alias('member_id'),
         F.col('mem_prv_id_align'),
         F.col('assignment_indicator'),
@@ -591,7 +618,7 @@ def _build_client_all_member(ngalign_df: DataFrame, mngreb_df: DataFrame,
         F.col('bene_usps_state_code_id').alias('mem_addr_state'),
         F.col('mem_addr_zip'),
         F.col('mem_report_hier_1'),
-        F.col('npi_prv_name_with_nulls').alias('mem_report_hier_2'),
+        F.coalesce(F.col('prv_name'), F.col('mem_prv_id_align')).alias('mem_report_hier_2'),
         F.col('mem_report_hier_3'),
         F.col('mem_report_hier_4'),
         F.col('mem_report_hier_5'),
@@ -708,6 +735,8 @@ def main() -> int:
 
     npi_df = sparkapp.load_df(PRM_META['path_product_ref'] / (PRM_META['filename_sas_npi'] + '.parquet'))
 
+    taxonomy_xref_df = sparkapp.load_df(PRM_META[15, 'out'] / 'ref_tax_spec_xwalk.parquet')
+
     next_gen_config_path = IndyPyPath(__file__).parent / 'next_gen_config.json'
     with next_gen_config_path.open() as json_file:
         next_gen_config = json.load(json_file)
@@ -715,12 +744,15 @@ def main() -> int:
 
     LOGGER.info('Loading NGALIGN files')
     alignment_df = _load_ngalign_files(sparkapp, ngalign_list, possible_delimiters, xref_df)
+    derived_performance_year = derive_performance_year(alignment_df)
 
     LOGGER.info('Loading MNGREB files')
     mngreb_df = _load_mngreb_files(sparkapp, mngreb_list, next_gen_config, possible_delimiters,
                                    xref_df)
 
-    client_member_list = _build_client_all_member(alignment_df, mngreb_df, phys_df, npi_df, next_gen_config)
+    client_providers_list = build_providers(phys_df, npi_df, taxonomy_xref_df,
+                                            derived_performance_year, next_gen_config)
+    client_member_list = _build_client_all_member(alignment_df, mngreb_df, client_providers_list)
 
     member_df = _create_member_df(client_member_list)
     member_time_df = _create_member_time_df(client_member_list, derived_date_latestpaid)
@@ -734,45 +766,46 @@ def main() -> int:
     structtypes = client_references.generate_structtypes()
 
     facility_structs = structtypes['client_facility']
-    provider_structs = structtypes['client_provider']
     
     mock_facility = mock_dataframe(sparkapp, facility_structs)
-    mock_provider = mock_dataframe(sparkapp, provider_structs)
     
     prm.spark.io_sas.export_dataframe(
         mock_facility,
         PRM_META[(18, 'out')] / 'client_facility.sas7bdat',
     )
     sparkapp.save_df(mock_facility, PRM_META[(18, 'out')] / 'client_facility.parquet')
-    
-    prm.spark.io_sas.export_dataframe(
-        mock_provider,
-        PRM_META[(18, 'out')] / 'client_provider.sas7bdat',
-    )
-    sparkapp.save_df(mock_provider, PRM_META[(18, 'out')] / 'client_provider.parquet')
 
     member_time_structs = structtypes['client_member_time']
     member_structs = structtypes['client_member']
+    provider_structs = structtypes['client_provider']
 
     member_time_updated_metadata = _update_metadata(member_time_structs, _MEMBER_TIME_METADATA)
     member_updated_metadata = _update_metadata(member_structs, _MEMBER_METADATA)
+    provider_updated_metadata = _update_metadata(provider_structs, _PROVIDER_METADATA)
 
     remove_blank_field_labels(member_time_updated_metadata)
     remove_blank_field_labels(member_updated_metadata)
+    remove_blank_field_labels(provider_updated_metadata)
 
     member_all_fill_df = member_df.na.fill(_NA_FILL)
 
     member_time_fill_df = member_time_df.select(
-        [F.col(field.name).cast(field.dataType).aliasWithMetadata(field.name,
-                                                                  metadata=field.metadata)
+        [F.col(field.name).cast(field.dataType).aliasWithMetadata(
+            field.name,metadata=field.metadata)
          for field in member_time_updated_metadata]
     ).orderBy(F.col('member_id'), F.col('date_start'))
 
     member_fill_df = member_all_fill_df.select(
-        [F.col(field.name).cast(field.dataType).aliasWithMetadata(field.name,
-                                                                  metadata=field.metadata)
+        [F.col(field.name).cast(field.dataType).aliasWithMetadata(
+            field.name, metadata=field.metadata)
          for field in member_updated_metadata]
     ).orderBy(F.col('member_id'))
+
+    client_provider_df = client_providers_list.select(
+        [F.col(field.name).cast(field.dataType).aliasWithMetadata(
+            field.name, metadata=field.metadata)
+         for field in provider_updated_metadata]
+    )
 
     prm.spark.io_sas.export_dataframe(
         member_fill_df,
@@ -785,6 +818,12 @@ def main() -> int:
         PRM_META[(18, 'out')] / 'client_member_time.sas7bdat',
     )
     sparkapp.save_df(member_time_fill_df, PRM_META[(18, 'out')] / 'client_member_time.parquet')
+
+    prm.spark.io_sas.write_sas_data(
+        client_provider_df,
+        PRM_META[18, 'out'] / 'client_provider.sas7bdat'
+    )
+    sparkapp.save_df(client_provider_df, PRM_META[18, 'out'] / 'client_provider.parquet')
 
     xref_df.unpersist()
     phys_df.unpersist()
